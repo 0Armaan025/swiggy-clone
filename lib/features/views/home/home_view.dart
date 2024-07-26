7ee8dab5af2a0270db8fb50f62e38d2583c731e7
Armaan <armaan33000@gmail.com>
2024-07-26 23:02:25 +0530

    getting height error

--- lib/features/views/home/home_view.dart
+++ lib/features/views/home/home_view.dart
@@ -1,21 +1,11 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeScreen(),
-    );
-  }
-}
-
 class HomeScreen extends StatelessWidget {
+  final _searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,6 +33,18 @@ class HomeScreen extends StatelessWidget {
 }
 
 class CustomHeaderDelegate extends SliverPersistentHeaderDelegate {
+  final List<String> imgList = [
+    'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+    'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+    'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+    'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+  ];
+
+  List<int> list = [1, 2, 3, 4, 5];
+
+  final _searchController = TextEditingController();
   @override
   Widget build(
       BuildContext context, double shrinkOffset, bool overlapsContent) {
@@ -50,47 +52,92 @@ class CustomHeaderDelegate extends SliverPersistentHeaderDelegate {
       margin: const EdgeInsets.only(
         top: 6,
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: Column(
         children: [
-          Column(
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                // crossAxisAlignment: CrossAxisAlignment.start,
+              Column(
                 children: [
-                  Icon(
-                    Icons.navigation_sharp,
-                    size: 30,
-                    color: HexColor("fb6018"),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.navigation_sharp,
+                        size: 30,
+                        color: HexColor("fb6018"),
+                      ),
+                      Text(
+                        "Some name",
+                        style: GoogleFonts.poppins(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.arrow_drop_down_sharp, size: 40),
+                      ),
+                    ],
                   ),
-                  Text(
-                    "Some name",
-                    style: GoogleFonts.poppins(
-                      color: Colors.black,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
+                  Padding(
+                    padding: const EdgeInsets.only(left: 14.0),
+                    child: Text(
+                      "some random boring address...",
+                      style: GoogleFonts.poppins(color: Colors.grey[500]),
                     ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.arrow_drop_down_sharp, size: 40),
-                  ),
+                  )
                 ],
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 14.0),
-                child: Text(
-                  "some random boring address",
-                  style: GoogleFonts.poppins(color: Colors.grey[500]),
+                padding: const EdgeInsets.only(left: 50.0, top: 20),
+                child: Column(
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    const CircleAvatar(
+                      radius: 25,
+                      backgroundImage:
+                          NetworkImage('https://via.placeholder.com/150'),
+                    ),
+                  ],
                 ),
-              )
+              ),
             ],
           ),
-          Column(
-            children: [],
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 16),
+            child: TextFormField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                hintText: "Search for products...",
+                hintStyle: GoogleFonts.poppins(color: Colors.grey[500]),
+                suffixIcon: Icon(
+                  Icons.search,
+                  color: Colors.grey[500],
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+            ),
           ),
+          Container(
+              child: CarouselSlider(
+            options: CarouselOptions(),
+            items: imgList
+                .map((item) => Container(
+                      child: Center(
+                          child: Image.network(item,
+                              fit: BoxFit.cover, width: 1000)),
+                    ))
+                .toList(),
+          )),
         ],
       ),
     );

3f767eb767f466f95a1a8aef99c5013ca7b88d43
Armaan <armaan33000@gmail.com>
2024-07-27 17:06:45 +0530

    not properly working, help

--- lib/features/views/home/home_view.dart
+++ lib/features/views/home/home_view.dart
@@ -1,13 +1,23 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 class HomeScreen extends StatelessWidget {
   final _searchController = TextEditingController();
+  final List<String> imgList = [
+    'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+    'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+    'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+    'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+  ];
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
         child: CustomScrollView(
@@ -16,6 +26,68 @@ class HomeScreen extends StatelessWidget {
               delegate: CustomHeaderDelegate(),
               pinned: false,
               floating: true,
+            ),
+            SliverToBoxAdapter(
+              child: Container(
+                width: double.infinity,
+                height: 400,
+                child: CarouselSlider(
+                  options: CarouselOptions(
+                    height: size.height * 0.22,
+                    autoPlay: true,
+                    enlargeCenterPage: true,
+                  ),
+                  items: imgList.map((imgUrl) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return Container(
+                          height: size.height * 0.2,
+                          width: MediaQuery.of(context).size.width,
+                          padding: const EdgeInsets.all(8),
+                          margin: EdgeInsets.symmetric(horizontal: 5.0),
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: NetworkImage(imgUrl),
+                              fit: BoxFit.cover,
+                            ),
+                            gradient: LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: [
+                                HexColor("#e7dddf"),
+                                HexColor("#f9f9f9"),
+                              ],
+                            ),
+                            borderRadius: BorderRadius.circular(6.0),
+                          ),
+                        );
+                      },
+                    );
+                  }).toList(),
+                ),
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: Container(
+                width: double.infinity,
+                height: 400,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 200, // Adjust width as needed
+                        color: Colors.red,
+                      ),
+                      Container(
+                        width: 200, // Adjust width as needed
+                        color: Colors.blue,
+                      ),
+                      // Add more containers or widgets here
+                    ],
+                  ),
+                ),
+              ),
             ),
             SliverList(
               delegate: SliverChildBuilderDelegate(
@@ -33,25 +105,14 @@ class HomeScreen extends StatelessWidget {
 }
 
 class CustomHeaderDelegate extends SliverPersistentHeaderDelegate {
-  final List<String> imgList = [
-    'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-    'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-    'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
-    'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
-    'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
-  ];
-
-  List<int> list = [1, 2, 3, 4, 5];
-
   final _searchController = TextEditingController();
+
   @override
   Widget build(
       BuildContext context, double shrinkOffset, bool overlapsContent) {
+    final size = MediaQuery.of(context).size;
     return Container(
-      margin: const EdgeInsets.only(
-        top: 6,
-      ),
+      margin: const EdgeInsets.only(top: 6),
       child: Column(
         children: [
           Row(
@@ -94,7 +155,6 @@ class CustomHeaderDelegate extends SliverPersistentHeaderDelegate {
               Padding(
                 padding: const EdgeInsets.only(left: 50.0, top: 20),
                 child: Column(
-                  // mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
                     const CircleAvatar(
@@ -127,27 +187,16 @@ class CustomHeaderDelegate extends SliverPersistentHeaderDelegate {
               ),
             ),
           ),
-          Container(
-              child: CarouselSlider(
-            options: CarouselOptions(),
-            items: imgList
-                .map((item) => Container(
-                      child: Center(
-                          child: Image.network(item,
-                              fit: BoxFit.cover, width: 1000)),
-                    ))
-                .toList(),
-          )),
         ],
       ),
     );
   }
 
   @override
-  double get maxExtent => 200.0;
+  double get minExtent => 180.0;
 
   @override
-  double get minExtent => 200.0;
+  double get maxExtent => 200.0;
 
   @override
   bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
